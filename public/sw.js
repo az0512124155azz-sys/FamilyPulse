c452d95c347fb6ce4e83dcc6d3497f7e403a00b7
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-20 00:01:51 +0300

    Refresh PWA cache for complete mobile layout update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='familypulse-v4';
+const CACHE='familypulse-v5';
 const CORE=['/','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
