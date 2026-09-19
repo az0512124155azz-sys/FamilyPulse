a4618137b6f6f1dec7b38ac5bb5968537f4fbb10
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-19 20:01:12 +0300

    Add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE='familypulse-v1';
+const CORE=['/','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const clone=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,clone)).catch(()=>{});
+        return response;
+      })
+      .catch(()=>caches.match(event.request).then(r=>r||caches.match('/')))
+  );
+});
