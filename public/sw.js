b70ea871b9ab4579d65b3fdf45b614cf4366ed0f
Avishai Tzabary <az0512124155azz@gmail.com>
2026-09-20 08:11:29 +0300

    Refresh PWA cache for legal pages

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='familypulse-v6';
+const CACHE='familypulse-v7';
 const CORE=['/','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
